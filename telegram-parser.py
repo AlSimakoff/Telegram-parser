9f273fc7f33c61f797f2809c85060b17a73fca9c
AlSimakoff <95354645+AlSimakoff@users.noreply.github.com>
2022-08-24 11:42:37 +0300

    Update telegram-parser.py

--- telegram-parser.py
+++ telegram-parser.py
@@ -1,17 +1,18 @@
-﻿from telethon.sync import TelegramClient
+from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, types, utils
+import logging
+import asyncio
 
 API_ID = **** #Your API_ID
 API_HASH = **** #Your API_Hash
 SESSION_STRING = '****' #Your Session Key
 
-
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
-
 client.start()
-# Выбираем канал назначения
-channel = client.get_entity('https://t.me/news_f_S')
+
+# target channel
+channel = types.PeerChannel(1652645965)
 
 channel1 = client.get_entity('https://t.me/wargonzo')
 channel2 = client.get_entity('https://t.me/dvachannel')
@@ -23,14 +24,30 @@ channel7 = client.get_entity('https://t.me/Cbpub')
 source_channel = [channel1, channel2, channel3,
                   channel4, channel5, channel6, channel7]
 
-
-
-
+id=[]
+@client.on(events.Album(chats=(source_channel)))
+async def handler(event):
+    global id
+    id=[]
+    for i in range(len(event.messages)):
+        id.append(event.messages[i].id)
+    await client.send_file(channel, file=event.messages, caption=event.text)
 
 @client.on(events.NewMessage(chats=(source_channel)))
 async def normal_handler(event):
-    await client.send_message(channel, event.message)
+    await asyncio.sleep(3)
+    global id
+    if event.message.id in id:
+        print('message from album', event.message.id)
+    else:
+        await client.send_message(channel, event.message)
+
+print('script is run')
 
 client.start()
+
 client.run_until_disconnected()
+
+
+
 
