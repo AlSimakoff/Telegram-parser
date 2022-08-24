e743ed74a01120a409e647e9ad0264dede316699
AlSimakoff <95354645+AlSimakoff@users.noreply.github.com>
2022-08-24 11:43:45 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
-﻿from telethon.sync import TelegramClient
+from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
 # Get Session key
-with TelegramClient(StringSession(), '****', '****'+7) as client: #Your API_ID and API_hash
+with TelegramClient(StringSession(), '****', '****',+7) as client: #Your API_ID, API_hash and phone number
     print(client.session.save())
